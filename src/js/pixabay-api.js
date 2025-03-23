0fc7cf30bff564f2bdd2ee6a8aff25806bf8990f
Alex Khavin <alexandr_havin@yahoo.com>
2025-03-23 13:45:30 +0200

    ready hw-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,14 +6,14 @@ export async function fetchData(options) {
     try {
         const response = await axios.get(`https://pixabay.com/api/?`, options);
         if (!response.data?.hits?.length) {
-               throw new Error("Sorry, there are no images matching your search query. Please try again!");
+            throw new Error("Sorry, there are no images matching your search query. Please try again!");
            }
            return response.data.hits;
     } catch (error) {
-        iziToast.show({
+        iziToast.error({
             message: `${error.message}`,
             messageColor: 'white',
-            iconUrl: '/blocked.svg',
+            // iconUrl: '/blocked.svg',
             position: 'topRight',
             color: '#ef4040',
         });
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 let inputData = "";
+let page = 1;
+let totalPages = 1;
 
 const options = {
     params: {
@@ -14,6 +16,8 @@ const options = {
         image_type: "photo",
         orientation: "horizontal",
         safesearch: true,
+        per_page: 15,
+        page: page,
         }
 };
 
@@ -21,10 +25,12 @@ const formData = document.querySelector(".form");
 const formInput = document.querySelector("input");
 const galleryList = document.querySelector(".gallery");
 const loader = document.querySelector(".loader");
+const btnLoadMore = document.querySelector(".btn");
+
 
 formData.addEventListener("submit", formSubmit);
 formInput.addEventListener("input", onInput);
-
+btnLoadMore.addEventListener("click", loadMore);
 
 function onInput(event) {
     inputData = event.target.value.trim();
@@ -33,12 +39,15 @@ function onInput(event) {
 async function formSubmit(event) {
     event.preventDefault();
     const form = event.currentTarget;
+    btnLoadMore.classList.add("hidden");
+    page = 1;
+    options.params.page = page;
 
     if (inputData === "") {
-         iziToast.show({
+         iziToast.error({
                 message: 'Fill in the search field!',
                 messageColor: 'white',
-                iconUrl: '/blocked.svg',
+                // iconUrl: '/blocked.svg',
                 position: 'topRight',
                 color: '#ef4040',
          });
@@ -53,18 +62,62 @@ async function formSubmit(event) {
 
     try {
         const images = await fetchData(options);
+           if (images.length === 0) {
+            btnLoadMore.classList.add("hidden");
+            return;
+           }
+        
+        totalPages = Math.ceil(500 / options.params.per_page);
+
         galleryList.insertAdjacentHTML("beforeend", markupImg(images));
+        btnLoadMore.classList.remove("hidden");
         gallery.refresh();
+
+        if (page < totalPages) {
+            btnLoadMore.classList.remove("hidden");
+        }
     } finally {
         loader.classList.add("hidden");
     }
 
     form.reset();
     inputData = "";
-}
+};
 
+async function loadMore() {
+    page += 1;
+    options.params.page = page;
+    loader.classList.remove("hidden");
 
+    if (page > totalPages) {
+        iziToast.error({
+            position: "topRight",
+            message: "We're sorry, but you've reached the end of search results.",
+            messageColor: 'white',
+            color: '#ef4040',
+        });
+        loader.classList.add("hidden");
+        btnLoadMore.classList.add("hidden");
+        return;
+    }
 
+  try {
+      const newImg = await fetchData(options);
+      galleryList.insertAdjacentHTML("beforeend", markupImg(newImg));
+      gallery.refresh();
+
+      const itemHeight = document.querySelector(".gallery-item");
+        if (itemHeight) {
+            const cardHeight = itemHeight.getBoundingClientRect().height;
+            window.scrollBy({ top: cardHeight * 2, behavior: "smooth" });
+        }
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+        loader.classList.add("hidden");
+    }
+};
 
 
 
